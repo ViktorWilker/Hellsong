574a59724eb275538a0fc49906340eb0f7338211
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2025-03-14 15:36:07 -0300

    Test: import animations

---
+++ Source/Hellsong/HMainCharacter.cpp
@@ -0,0 +1,22 @@
+﻿#include "HMainCharacter.h"
+
+AHMainCharacter::AHMainCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AHMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
