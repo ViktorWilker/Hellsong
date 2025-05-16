9dfaa5a62638adac430b16112dc984c3a5f694bb
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2025-05-16 17:25:36 -0300

    Stat system completed - regem stat

--- Source/Hellsong/ActorComponent/StatsComponent.cpp
+++ Source/Hellsong/ActorComponent/StatsComponent.cpp
@@ -1,7 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "StatsComponent.h"
+
+#include "TimerManager.h"
 
 UStatsComponent::UStatsComponent()
 {
@@ -28,13 +27,115 @@ void UStatsComponent::InitializeStats()
 	}
 }
 
+void UStatsComponent::ModifyCurrentStatValue(EStats stat, float value, bool bShoudRegen)
+{
+	if(value == 0) return;
+	
+	float l_value = GetCurrentStatValue(stat) + value;
+	float clampvalue = FMath::Clamp(l_value, 0.f, GetMaxStatValue(stat));
+	SetCurrentStatValue(stat, clampvalue);
+	if(bShoudRegen)
+	{
+		HandleDelayedRegen(stat);
+	}
+}
+
+void UStatsComponent::TakeDamage(float InDamage)
+{
+	ModifyCurrentStatValue(EStats::Health,InDamage * -1.0f, false);
+}
+
+void UStatsComponent::HandleDelayedRegen(EStats Stat)
+{
+	GetWorld()->GetTimerManager().ClearTimer(RegenTimerHandles.FindOrAdd(Stat));
+	GetWorld()->GetTimerManager().ClearTimer(RegenDelayHandles.FindOrAdd(Stat));
+	
+	FTimerDelegate DelayDelegate;
+	DelayDelegate.BindUObject(this, &UStatsComponent::StartRegen, Stat);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		RegenDelayHandles[Stat],
+		DelayDelegate,
+		1.0f,
+		false
+	);
+}
+
+void UStatsComponent::StartRegen(EStats StatType)
+{
+	FTimerDelegate RegenDelegate;
+	RegenDelegate.BindUObject(this, &UStatsComponent::RegenStat, StatType);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		RegenTimerHandles[StatType],
+		RegenDelegate,
+		0.1f,
+		true
+	);
+}
+
+void UStatsComponent::RegenStat(EStats Stat)
+{
+	
+	float Current = GetCurrentStatValue(Stat);
+	float Max = GetMaxStatValue(Stat);
+	if (Current >= Max)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(RegenTimerHandles[Stat]);
+		return;
+	}
+
+	float RegenAmount = GetRegenRate(Stat);
+	Current = FMath::Min(Current + RegenAmount, Max);
+	SetCurrentStatValue(Stat, Current);
+}
+
+
 void UStatsComponent::SetCurrentStatValue(EStats Stat, float value)
 {
-	CurrentStatValue.Add(Stat, value);
+	CurrentStatValue.FindOrAdd(Stat) = value;
+	OnStatChanged.Broadcast(Stat, value);
+}
+
+void UStatsComponent::SetBaseStatValue(EStats Stat, float value)
+{
+	if(FBaseStat* statstruct = BaseStats.Find(Stat))
+	{
+		statstruct->BaseValue = value;
+	}
+}
+
+void UStatsComponent::SetMaxStatValue(EStats Stat, float value)
+{
+	if(FBaseStat* statstruct = BaseStats.Find(Stat))
+	{
+		statstruct->MaxValue = value;
+	}
+}
+
+float UStatsComponent::GetRegenRate(EStats stat) const
+{
+	switch (stat)
+	{
+	case EStats::Mana:
+		return ManaRegenRate;
+		default:
+			return 0.f;
+	}
 }
 
 float UStatsComponent::GetCurrentStatValue(EStats Stat)
 {
 	return *CurrentStatValue.Find(Stat);
+}
+
+float UStatsComponent::GetBaseStatValue(EStats Stat)
+{
+	return BaseStats.Find(Stat)->BaseValue;
+}
+
+float UStatsComponent::GetMaxStatValue(EStats Stat)
+{
+	return BaseStats.Find(Stat)->MaxValue;
 }
 
--- Source/Hellsong/ActorComponent/StatsComponent.h
+++ Source/Hellsong/ActorComponent/StatsComponent.h
@@ -17,6 +17,8 @@ enum class EStats : uint8
 	Corruption UMETA(DisplayName = "Corruption")
 };
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStatChangedSignature, EStats, StatType, float, NewValue);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HELLSONG_API UStatsComponent : public UActorComponent
 {
@@ -25,22 +27,61 @@ class HELLSONG_API UStatsComponent : public UActorComponent
 public:	
 	UStatsComponent();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
+	
 	UFUNCTION(BlueprintCallable)
 	void InitializeStats();
+
+	UFUNCTION(BlueprintCallable)
+	void ModifyCurrentStatValue(EStats stat, float value, bool bShoudRegen);
+
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float InDamage);
+
+	UFUNCTION()
+	void HandleDelayedRegen(EStats Stat);
+	
+	UFUNCTION(BlueprintCallable)
+	void StartRegen(EStats StatType);
+	
+	UFUNCTION()
+	void RegenStat(EStats Stat);
 	
 	UFUNCTION(BlueprintCallable)
 	void SetCurrentStatValue(EStats Stat, float value);
 
+	UFUNCTION(BlueprintCallable)
+	void SetBaseStatValue(EStats Stat, float value);
+
+	UFUNCTION(BlueprintCallable)
+	void SetMaxStatValue(EStats Stat, float value);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetRegenRate(EStats stat) const;
+
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 	float GetCurrentStatValue(EStats Stat);
 
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetBaseStatValue(EStats Stat);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxStatValue(EStats Stat);
+	
 	UPROPERTY(EditAnywhere, Category = "BaseStats")
 	TMap<EStats, FBaseStat> BaseStats;
+
+	UPROPERTY(BlueprintAssignable, Category="Stats")
+	FOnStatChangedSignature OnStatChanged;
+	
 	
 protected:
 	virtual void BeginPlay() override;
-
+	FTimerHandle RegenTimerHandle;
+	
 private:
-	TMap<EStats, float> CurrentStatValue; 
+	TMap<EStats, float> CurrentStatValue;
+	TMap<EStats, FTimerHandle> RegenTimerHandles;
+	TMap<EStats, FTimerHandle> RegenDelayHandles;
+	bool bCanRegenerate = false;
+	float ManaRegenRate = 2.f;
 };
